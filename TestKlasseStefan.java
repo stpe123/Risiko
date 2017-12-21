c6fac9b5ff334900c1343a670f219b53b7f02aad
stpe123 <stevepe93@gmail.com>
2017-12-21 13:40:46 +0100

    Keine wirklichen Änderungen

--- TestKlasseStefan.java
+++ TestKlasseStefan.java
@@ -1,13 +1,17 @@
 /**
   *
-  * Beschreibung
+  * Testfälle Stefan
   *
   * @version 1.0 vom 21.12.2017
-  * @author 
+  * @Stefan
   */
 
 public class TestKlasseStefan {
-      //Würfel testen
+  
+  
+  // Anfang Methoden
+  public void KlassenTesten() {
+    //Würfel testen
     int WAugenzahl;
     Wuerfel Wuerfel1 = new Wuerfel();
     WAugenzahl = Wuerfel1.getAugenzahl();
@@ -18,14 +22,8 @@ public class TestKlasseStefan {
     Spieler Stefan = new Spieler("Stefan");
     m = Stefan.angreifen(); 
     System.out.println(m);
-  
-  // Anfang Attribute
-  // Ende Attribute
-  
-  // Anfang Methoden
-  public void KlassenTesten() {
     
   }
-
+  
   // Ende Methoden
 } // end of TestKlasseStefan
