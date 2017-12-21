a80c6563e943493d966d58ad67d18735392f4d6b
MrWh1t3-h-brs <Mr.Wh1t3@web.de>
2017-12-21 13:08:51 +0100

    Änderungen

---
+++ Risiko.java
@@ -0,0 +1,20 @@
+/**
+  *
+  * Beschreibung
+  *
+  * @version 1.0 vom 21.12.2017
+  * @author 
+  */
+
+public class Risiko {
+  
+  // Anfang Attribute
+  // Ende Attribute
+  
+  // Anfang Methoden
+  public void main(String[] args) {
+    
+  }
+
+  // Ende Methoden
+} // end of Risiko
