1900987921af10dc0a07fe272ad8e9cda66e5d51
MrWh1t3-h-brs <Mr.Wh1t3@web.de>
2018-01-03 10:57:45 +0100

    Sachen geändert

--- Risiko.java
+++ Risiko.java
@@ -17,23 +17,31 @@ public class Risiko {
   public static void main(String[] args) {
     
     //********************************Initialisierungsphase*********************************************************
-    Spieler Spieler1 = new Spieler("Alex");
-    Spieler Spieler2 = new Spieler("Stefan");
-    Spieler AktuellerSpieler= new Spieler("AktuellerSpieler");
-    Verwaltung VW = new Verwaltung(Spieler1,Spieler2);
-    Weltkarte Erde = new Weltkarte();
-    //Testframe test = new Testframe("Risiko");
+    //Alle Objekte anlegen
+    Spieler[] spieler = new Spieler[2];
+    spieler[0] = new Spieler("Stefan");
+    spieler[1] = new Spieler("Alex");
+    Spieler AktuellerSpieler;
+    Verwaltung VW = new Verwaltung(spieler);
+    Weltkarte Erde = new Weltkarte();    
     
-    
+    //Verwaltung verteilt die Gebietskarten an Spieler
     VW.GebieteEinsammeln(Erde.getGebiete());
     VW.GebieteMischen();
+    VW.GebieteGleichmaessigAnSpielerVerteilen();
     
-    Spieler1.GebieteAnnehmen(VW.GebieteUebergeben(2));
-    Spieler2.GebieteAnnehmen(VW.GebieteUebergeben(1));
+    //Verteilen der Gebiete an alle Spieler
+    spieler[0].GebieteAnnehmen(VW.GebieteUebergeben(2));
+    spieler[1].GebieteAnnehmen(VW.GebieteUebergeben(1));
     
-    Spieler1.TruppenSetzen(10);
-    Spieler2.TruppenSetzen(10);
     
+    
+    
+    
+    //Jeder Spieler darf am Anfang eine Feste Zahl an Truppen verteilen
+    for ( int i = 0; i < spieler.length; i++ ){
+      spieler[i].TruppenSetzen(VW.getErlaubteTruppenzahlInInitialisierungsphase());
+    }
     
     
     
